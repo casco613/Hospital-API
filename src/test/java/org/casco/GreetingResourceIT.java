cc9ddbc865b513b1871318e75a86f98c814b8d2e
quarkusio <no-reply@quarkus.io>
2025-05-05 00:58:08 +0000

    Initial commit

---
+++ src/test/java/org/casco/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.casco;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
